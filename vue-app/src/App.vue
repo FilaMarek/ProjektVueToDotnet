5ef960f0847e0825b92ace6407ce7c3a7f3b0e0b
FilaMarek <48298031+FilaMarek@users.noreply.github.com>
2021-10-11 14:44:16 +0200

    restarted the project

---
+++ vue-app/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="app">
+<side-menu>
+
+</side-menu>
+
+</div>
+
+<!--  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
+  <router-view />
+</template>
+
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import SideMenu from "@/components/SideMenu.vue"; // @ is an alias to /src
+
+@Options({
+  name: 'Home',
+  components: {SideMenu}
+
+
+  //components: {
+  //  SideMenu,
+  //},
+})
+export default class Home extends Vue {}
+</script>
+
+
+
+
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ vue-app/src/components/SideMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="Side-Menu-container">
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+
+name:'SideMenu',
+components: {}
+})
+
+export default class SideMenu extends Vue { }
+</script>
+
+
+<style scoped lang="scss">
+@import "@/scss/global.scss";
+.Side-Menu-container {
+  background-color: #f7f7f7;
+  height: 100vh;
+  width: $menu-width;
+  display: flex;
+  flex-direction: column;
+  padding: 0.8rem;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.12), 0 1px 1px rgba(0, 0, 0, 0.24);
+  box-sizing: border-box;
+}
+
+</style>>
